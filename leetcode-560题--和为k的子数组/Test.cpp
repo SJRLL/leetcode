03abfb298914cb97cc4b76d11694dffbc9a0853e
SJRLL <1124335040@qq.com>
2020-02-26 20:17:46 +0800

    leetcode-560题--和为k的子数组

---
+++ leetcode-560题--和为k的子数组/Test.cpp
@@ -0,0 +1,31 @@
+﻿给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+
+示例 1 :
+
+输入 : nums = [1, 1, 1], k = 2
+输出 : 2, [1, 1] 与[1, 1] 为两种不同的情况。
+
+class Solution {
+public:
+	int subarraySum(vector<int>& nums, int k) {
+		int count = 0;
+		int len = nums.size();
+		if (len <= 0)
+		{
+			return 0;
+		}
+		vector<int> sum(len, 0);
+		for (int i = 0; i<len; ++i)
+		{
+			for (int j = 0; j <= i; ++j)
+			{
+				sum[j] += nums[i];
+				if (sum[j] == k)
+				{
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+};
